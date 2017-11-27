0bac3b415eb7042bf62f8af1b371a9fd3742cde2
Ben Cordero <bencord0@condi.me>
2017-11-27 22:55:18 +0000

    Update documentation

---
+++ implementors/log/trait.Log.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["loggerv"] = ["impl <a class=\"trait\" href=\"log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"loggerv/struct.Logger.html\" title=\"struct loggerv::Logger\">Logger</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
